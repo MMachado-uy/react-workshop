394ad75ff1ce85dad3ce9632af4794b671ee153c
Mauricio Machado <mauricio.machado@modelit.xyz>
2018-08-23 12:50:14 -0300

    02-Hola Mundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+import HolaMundo from './components/Hola.jsx'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <p>Hola Mundo!</p>
-      </div>
+      <HolaMundo/>
     );
   }
 }
---
+++ src/components/Hola.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HolaMundo = () => {
+    return ( 
+        <div>
+            <p>Hola Mundo!</p>
+        </div>
+    )
+}
+
+export default HolaMundo;
