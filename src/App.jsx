32e8538f77989fdd94d399f98dfbae80bcb230fc
Mauricio Machado <mauricio.machado@modelit.xyz>
2018-08-23 15:45:03 -0300

    04-Router mejorado

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,30 @@ import ReactDOM from "react-dom";
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
+import { Well } from 'react-bootstrap';
 
-import HolaMundo from './components/Hola.jsx'
-import Home from './components/Home.jsx'
-import NotFound from './components/NotFound.jsx';
+import HolaMundo  from './components/Hola.jsx'
+import Home       from './components/Home.jsx'
+import NotFound   from './components/NotFound.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Well>
         <Router>
           <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/hola" component={HolaMundo}/>
-            <Route component={NotFound}/>
+            <Route exact path="/404" component={NotFound}/>
+            <Route>
+              <Redirect to="/404"/>
+            </Route>
           </Switch>
         </Router>
-      </div>
+      </Well>
     );
   }
 }
