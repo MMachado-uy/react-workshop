4aa1ffa9a4a26edaffc956d4c1f3dd27a6da0f48
Mauricio Machado <macmauricio@gmail.com>
2018-10-24 17:59:26 -0300

    08-Handling state

--- src/components/Hola.jsx
+++ src/components/Hola.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
 
-const HolaMundo = (props) => {
+class HolaMundo extends React.Component {
 
-    let { quien, mensaje } = props;
+    constructor(props) {
+        super(props);
+        this.state = {
+            contador: 0
+        }
 
-    return ( 
-        <div>
-            <p>Hola {quien || 'Fallback'}!</p>
+        this.setCount = this.setCount.bind(this);
+    }
 
-            {typeof mensaje !== 'undefined' &&
-                <div>{mensaje}</div>
-            }
-        </div>
-    )
+    setCount() {
+        let val = this.state.contador;
+        val++
+
+        this.setState({
+            contador: val
+        }, () => {
+            // Callback
+        })
+    }
+    
+    render() {
+        let { quien, mensaje } = this.props;
+        let { contador } = this.state;
+
+        return ( 
+            <div>
+                <p>Hola {quien || 'Fallback'}!</p>
+
+                {typeof mensaje !== 'undefined' &&
+                    <div>{mensaje}</div>
+                }
+                <input type="button" onClick={this.setCount} value="Click acá"/>
+                
+                {contador > 0 &&
+                    <div>Clickearon {contador} veces!</div>
+                }
+            </div>
+        )
+    }
 }
 
 export default HolaMundo;
