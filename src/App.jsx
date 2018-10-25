8ddc209298844d43f2ef12c445c1f0194b2fed9f
Mauricio Machado <mauricio.machado@modelit.xyz>
2018-10-25 18:25:06 -0300

    09-Agregado Componente Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,46 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
 } from 'react-router-dom';
 
-import HolaMundo  from './components/Hola.jsx'
-import Home       from './components/Home.jsx'
+import HolaMundo  from './components/Hola.jsx';
+import Home       from './components/Home.jsx';
 import NotFound   from './components/NotFound.jsx';
+import Login      from './components/Login.jsx';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/hola" component={HolaMundo}/>
-          <Route exact path="/404" component={NotFound}/>
-          <Route>
-            <Redirect to="/404"/>
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            usuario: ''
+        }
+    }
+    
+    render() {
+        let { usuario } = this.state;
+
+        return (
+            <Router>
+                { usuario != '' ?
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/hola" component={HolaMundo}/>
+                        <Route exact path="/404" component={NotFound}/>
+                        <Route>
+                            <Redirect to="/404"/>
+                        </Route>
+                    </Switch>
+                    :
+                    <Login/>
+                }
+            </Router>
+            );
+        }
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Panel } from 'react-bootstrap';
+
+export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+
+        let {
+            id,
+            value
+        } = event.target;
+
+        let newState = {};
+
+        newState[id] = value;
+
+        this.setState(newState, () => {});
+    }
+
+    handleSubmit(event) {
+
+        event.preventDefault();
+
+        let {
+            username,
+            password
+        } = this.state;
+
+        let req = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        };
+
+        fetch('http://54.215.223.178:8080/api/login', req)
+        .then(res =>  res.json())
+        .then(data => {
+            if (typeof res !== 'undefined')
+            console.log(data.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    render() {
+        return (
+            <Panel>
+                <Panel.Heading>
+                    <Panel.Title componentClass="h3">Login</Panel.Title>
+                </Panel.Heading>
+                <Panel.Body>
+                    <form>
+                        <label htmlFor="username">User: </label>
+                        <input type="text" id="username" onChange={this.handleChange}/>
+                        <label htmlFor="password">Password: </label>
+                        <input type="password" id="password" onChange={this.handleChange}/>
+                        <input type="button" value="Log In" onClick={this.handleSubmit}/>
+                    </form>
+                </Panel.Body>
+            </Panel>
+        );
+    }
+};
